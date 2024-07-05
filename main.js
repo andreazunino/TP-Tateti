a342df787d36525101b0e72c677dfb4992665959
Andrea <andrea.zunino@tsoftglobal.com>
2024-07-05 09:09:48 -0300

    Se acomoda el titulo

--- main.js
+++ main.js
@@ -1,27 +1,28 @@
+// se inicia el tablero y el primer jugador
 let board = ["", "", "", "", "", "", "", "", ""];
 let currentPlayer = "X";
-const cells = document.querySelectorAll("td");
-let currentCell;
+const cells = document.querySelectorAll("td"); //contiene todas las celdas del tablero
+let currentCell; //almacena la celda actual
 
 // URL de la API del proyecto en mockapi
 fetch('https://666c325d49dbc5d7145d127c.mockapi.io/preguntas')
     .then(response => response.json())
     .then(data => {
-        window.questions = data;
+        window.questions = data; //se almacenan en wind quest
         console.log('Preguntas obtenidas:', data); // Verifica que las preguntas se obtienen correctamente
     });
 
 cells.forEach((cell, index) => {
-    cell.addEventListener("click", () => handleClick(index));
+    cell.addEventListener("click", () => handleClick(index)); // se agrega el evento a cada celda para manejar los clicks
 });
 
 function handleClick(index) {
-    if (board[index] !== "") return;
+    if (board[index] !== "") return; //si la celda esta ocupada no hace nada,sino almacena el indice de la celda actual y llama a showQuestion para mostrar una pregunta.
     currentCell = index;
     showQuestion();
 }
 
-function showQuestion() {
+function showQuestion() { //selecciona una pregunta aleatoria
     let question = window.questions[Math.floor(Math.random() * window.questions.length)];
     document.getElementById('questionText').textContent = question.Pregunta;
     
@@ -39,7 +40,7 @@ function showQuestion() {
     $('#questionModal').modal('show');
 }
 
-function checkAnswer(question, selected) {
+function checkAnswer(question, selected) { //verifica si la respuesta es correcta, se actualiza el tablero y la celda correspondiente con el jugador actual y se verifica si hay un ganador. Si la respuesta es incorrecta, se muestra una alerta
     $('#questionModal').modal('hide');
     if (question.Verdadera === selected) {
         board[currentCell] = currentPlayer;
@@ -53,7 +54,7 @@ function checkAnswer(question, selected) {
     }
 }
 
-function checkWin() {
+function checkWin() { //verifica si las condiciones para ganar se cumplen
     const winConditions = [
         [0, 1, 2], [3, 4, 5], [6, 7, 8], // Horizontales
         [0, 3, 6], [1, 4, 7], [2, 5, 8], // Verticales
